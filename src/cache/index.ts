f3112817429f45750682c5424bde7cde146fbd6d
nubro999 <zcb167@gmail.com>
2025-03-11 18:46:09 +0900

    WSL contegration

--- src/cache/index.ts
+++ src/cache/index.ts
@@ -8,3 +8,6 @@ export function initializeDbCache(
   const cache = new CacheManager(new DbCacheAdapter(db, character.id));
   return cache;
 }
+
+//node 언어 배우기, WSL을 VS코드에 통합하기, node.js 설치하기, npm
+//node.js는 자바스크립트 런타임 환경이다. 자바스크립트를 실행할 수 있는 환경을 제공한다.
